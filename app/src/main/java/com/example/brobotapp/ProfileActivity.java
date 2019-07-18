fd9f049f2d40727ca02c03b76a8666f4ba99403c
Kzrthikz <mdkkarthika@gmail.com>
2019-07-18 14:30:28 +0800

    Test Commit

--- app/src/main/java/com/example/brobotapp/ProfileActivity.java
+++ app/src/main/java/com/example/brobotapp/ProfileActivity.java
@@ -1,10 +1,12 @@
 package com.example.brobotapp;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.applozic.mobicommons.commons.core.utils.Utils;
@@ -24,6 +26,9 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     //view objects
     private TextView textViewUserEmail;
+    private ImageView chatbot;
+    private ImageView relaxationStrategies;
+    private ImageView todo;
     private Button buttonLogout;
 
 
@@ -50,12 +55,18 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         //initializing views
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        chatbot = (ImageView) findViewById(R.id.chatbot) ;
+        relaxationStrategies = (ImageView) findViewById(R.id.relaxtionStrategies);
+        todo = (ImageView) findViewById(R.id.todo);
 
         //displaying logged in user name
         textViewUserEmail.setText("Welcome " + user.getEmail());
 
         //adding listener to button
         buttonLogout.setOnClickListener(this);
+        chatbot.setOnClickListener(this);
+        relaxationStrategies.setOnClickListener(this);
+        todo.setOnClickListener(this);
         Kommunicate.init(this, "3cb2ef8fe32b5a1e6ea390c04a81822");
     }
 
@@ -70,7 +81,27 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             //starting login activity
             startActivity(new Intent(this, LoginActivity.class));
         }
-        new KmChatBuilder(this).launchChat(new KmCallback() {
+        if(view == chatbot) {
+            new KmChatBuilder(this).launchChat(new KmCallback() {
+                @Override
+                public void onSuccess(Object message) {
+                    Utils.printLog(ProfileActivity.this, "ChatTest", "Success : " + message);
+                }
+
+                @Override
+                public void onFailure(Object error) {
+                    Utils.printLog(ProfileActivity.this, "ChatTest", "Failure : " + error);
+                }
+            });
+        }
+        if(view == relaxationStrategies) {
+            startActivity(new Intent(this, RelaxationActivity.class));
+        }
+        if(view == todo) {
+            startActivity(new Intent(this, ToDoActivity.class));
+        }
+
+        /*new KmChatBuilder(this).launchChat(new KmCallback() {
             @Override
             public void onSuccess(Object message) {
                 Utils.printLog(ProfileActivity.this, "ChatTest", "Success : " + message);
@@ -80,7 +111,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             public void onFailure(Object error) {
                 Utils.printLog(ProfileActivity.this, "ChatTest", "Failure : " + error);
             }
-        });
+        });*/
     }
 
 
